632a687b206b2fbb80450745d8637009e72762c9
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2025-03-27 12:40:29 +0300

    WebAssembly

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@
 go build -o .bin/server sokoban/cmd/server
 set GOOS=js
 set GOARCH=wasm
-go build -o cmd/server/resources/client.wasm cmd/client/main.go
+go build -o cmd/server/resources/client.wasm sokoban/cmd/client
