1da12332710b0c1ef329f1444327d0c3804cef9d
Хлебников Андрей Александрович <andrey.khlebnikov@synchro.pro>
2025-03-27 12:28:57 +0300

    WebAssembly

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,18 +2,30 @@
 
 package main
 
-import "syscall/js"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 ///link: https://stdiopt.github.io/gowasm-experiments/bouncy/
 ///link: https://stdiopt.github.io/gowasm-experiments/hexy/
 ///link: https://github.com/stdiopt/gowasm-experiments
 ///link: https://github.com/justinclift/tinygo_canvas2/blob/master/wasm.go
 
-func main() {
+//go:wasmexport access
+func access(token string) {
+	append(fmt.Sprintf("Access token is '%s'", token))
+}
+
+func append(text string) {
 	document := js.Global().Get("document")
 	body := document.Call("querySelector", "body")
 	div := document.Call("createElement", "div")
-	div.Set("innerHTML", "Hello, WASM")
+	div.Set("innerHTML", text)
 	body.Call("appendChild", div)
+}
+
+func main() {
+	append("Started")
 	<-make(chan bool)
 }
